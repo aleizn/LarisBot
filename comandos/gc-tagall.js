0da273a9f186fa87715579eef2a74f3983e2c745
aleizn <119465229+aleizn@users.noreply.github.com>
2023-04-01 20:08:15 -0500

    Aleizn

--- comandos/gc-tagall.js
+++ comandos/gc-tagall.js
@@ -5,11 +5,11 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `Este es creado Wa.me/51992621601 no permito la reventa
+let teks = `@laris_desing_
 *⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `┣👻 @${mem.id.split('@')[0]}\n`}
-teks += `└𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 𝐀𝐥𝐞`
+teks += `┣🩵 @${mem.id.split('@')[0]}\n`}
+teks += `└𝐋𝐚𝐫𝐢𝐬.𝐅𝐱𝐜𝐱`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
